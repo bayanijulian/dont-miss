1e8a6e5746c7a51832cac930b1a513b30596df2f
Bayani Julian <bayanijulian@gmail.com>
2014-06-16 23:47:39 -0500

    the spawning class has been set up

--- core/src/com/hypetrainstudios/dontmiss/handlers/DifficultyHandler.java
+++
@@ -1,6 +0,0 @@
-package com.hypetrainstudios.dontmiss.handlers;
-
-public class DifficultyHandler 
-{
-	
-}
---
+++ core/src/com/hypetrainstudios/dontmiss/handlers/SpawnHandler.java
@@ -0,0 +1,66 @@
+package com.hypetrainstudios.dontmiss.handlers;
+
+import java.util.Random;
+
+public class SpawnHandler {
+	private static float spawnWaveRate = 3f;
+	private static float spawnWaveRateCounter = 0;
+	private static Random rdm = new Random();
+	private static void spawnAmountRdm(int amountToSpawn){
+		
+	}
+	private static void spawnAmountSpacedEqually(int amountToSpawn){
+		
+	}
+	private static void spawn4SpacedEqually(){
+		
+	}
+	private static void spawn6SpacedEqually(){
+		
+	}
+	private static void spawn8SpacedEqually(){
+		
+	}
+	
+	private static void challengeFor300(){
+		
+	}
+	private static void challengeFor245(){
+		
+	}
+	private static void challengeFor230(){
+		
+	}
+	private static void challengeFor215(){
+		
+	}
+	private static void challengeFor200(){
+		
+	}
+	private static void challengeFor145(){
+		
+	}
+	private static void challengeFor130(){
+		
+	}
+	private static void challengeFor115(){
+		
+	}
+	private static void challengeFor100(){
+		
+	}
+	private static void challengeFor045(){
+		
+	}
+	private static void challengeFor030(){
+		
+	}
+
+	private static void challengeFor015(){
+		
+	}
+
+	public static void update(float delta, float time){
+		
+	}
+}
