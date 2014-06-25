a48d1df3d786f1442098cb00efa171102d24971f
MattRychtarczyk <polskidude2@gmail.com>
2014-06-24 21:15:54 -0500

    Started Menu

--- core/src/com/hypetrainstudios/dontmiss/screens/MenuScreen.java
+++ core/src/com/hypetrainstudios/dontmiss/screens/MenuScreen.java
@@ -1,15 +1,58 @@
 package com.hypetrainstudios.dontmiss.screens;
 
+import javafx.stage.Stage;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Button.ButtonStyle;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.hypetrainstudios.dontmiss.handlers.AssetHandler;
 
 public class MenuScreen implements Screen{
-
+	private Stage stage;
+	private FitViewport view;
+	
+	private Button playBtn, optionsBtn, rightArrowBtn, leftArrowBtn;
+	private ButtonStyle playBtnStyle, optionsBtnStyle;
+	
+	private Image background;
+	
+	public MenuScreen(Game g) {
+		
+	}
+	
+	public void createPlayBtn() {
+		playBtnStyle = new ButtonStyle(new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("playBtn"))),
+									   new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("playBtnPressed"))),
+									   new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("playBtn"))));
+		playBtn = new Button(playBtnStyle);
+		playBtn.setPosition((Gdx.graphics.getWidth()/2-(playBtn.getWidth()/2)), ((Gdx.graphics.getHeight()/2)-(playBtn.getHeight()/2)));
+	}
+	
+	public void createOptionsBtn() {
+		optionsBtnStyle = new ButtonStyle(new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("optionsBtn"))),
+									   	  new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("optionsBtnPressed"))),
+									   	  new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("optionsBtn"))));
+		optionsBtn = new Button(optionsBtnStyle);
+		optionsBtn.setPosition((Gdx.graphics.getWidth()/2-(optionsBtn.getWidth()/2)), ((Gdx.graphics.getHeight()/2)-(optionsBtn.getHeight()/2)));
+	}
+	
+	public void createBackground() {
+		
+	}
+	
 	@Override
 	public void render(float delta) {
+		
 	}
 
 	@Override
 	public void resize(int width, int height) {
+		
 	}
 
 	@Override
