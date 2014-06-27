44d0653320a62adcad5585c5277e85d282a666e2
MattRychtarczyk <polskidude2@gmail.com>
2014-06-26 23:02:57 -0500

    Added more buttons to Menu

--- core/src/com/hypetrainstudios/dontmiss/screens/MenuScreen.java
+++ core/src/com/hypetrainstudios/dontmiss/screens/MenuScreen.java
@@ -16,8 +16,8 @@ public class MenuScreen implements Screen{
 	private Stage stage;
 	private FitViewport view;
 	
-	private Button playBtn, optionsBtn, rightArrowBtn, leftArrowBtn;
-	private ButtonStyle playBtnStyle, optionsBtnStyle;
+	private Button playBtn, optionsBtn, rightArrowBtn, leftArrowBtn, oneBtn, twoBtn, threeBtn;
+	private ButtonStyle playBtnStyle, optionsBtnStyle, rightArrowBtnStyle, leftArrowBtnStyle, oneBtnStyle, twoBtnStyle, threeBtnStyle;
 	
 	private Image background;
 	
@@ -39,6 +39,46 @@ public class MenuScreen implements Screen{
 									   	  new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("optionsBtn"))));
 		optionsBtn = new Button(optionsBtnStyle);
 		optionsBtn.setPosition((Gdx.graphics.getWidth()/2-(optionsBtn.getWidth()/2)), ((Gdx.graphics.getHeight()/2)-(optionsBtn.getHeight()/2)));
+	}
+	
+	public void createRightArrowBtn() {
+		rightArrowBtnStyle = new ButtonStyle(new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("rightArrowBtn-01"))),
+			   	  			 new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("rightArrowBtnPressed"))),
+			   	  			 new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("rightArrowBtn-01"))));
+		rightArrowBtn = new Button(rightArrowBtnStyle);
+		rightArrowBtn.setPosition((Gdx.graphics.getWidth()/2-(rightArrowBtn.getWidth()/2)), ((Gdx.graphics.getHeight()/2)-(rightArrowBtn.getHeight()/2)));
+	}
+	
+	public void createLeftArrowBtn() {
+		leftArrowBtnStyle = new ButtonStyle(new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("leftArrowBtn"))),
+  	  			 			new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("leftArrowBtnPressed"))),
+  	  			 			new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("leftArrowBtn"))));
+		leftArrowBtn = new Button(leftArrowBtnStyle);
+		leftArrowBtn.setPosition((Gdx.graphics.getWidth()/2-(leftArrowBtn.getWidth()/2)), ((Gdx.graphics.getHeight()/2)-(leftArrowBtn.getHeight()/2)));
+	}
+	
+	public void createOneBtn() {
+		oneBtnStyle = new ButtonStyle(new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("oneBtn"))),
+  	  			 	  new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("oneBtnPressed"))),
+  	  			 	  new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("oneBtn"))));
+		oneBtn = new Button(oneBtnStyle);
+		oneBtn.setPosition((Gdx.graphics.getWidth()/2-(oneBtn.getWidth()/2)), ((Gdx.graphics.getHeight()/2)-(oneBtn.getHeight()/2)));
+	}
+	
+	public void createTwoBtn() {
+		twoBtnStyle = new ButtonStyle(new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("twoBtn"))),
+ 	  			 	  new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("twoBtnPressed"))),
+ 	  			 	  new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("twoBtn"))));
+		twoBtn = new Button(twoBtnStyle);
+		twoBtn.setPosition((Gdx.graphics.getWidth()/2-(twoBtn.getWidth()/2)), ((Gdx.graphics.getHeight()/2)-(twoBtn.getHeight()/2)));
+	}
+	
+	public void createThreeBtn() {
+		threeBtnStyle = new ButtonStyle(new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("threeBtn"))),
+ 			 	  		new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("threeBtnPressed"))),
+ 			 	  		new TextureRegionDrawable((AssetHandler.manager.get(AssetHandler.atlasButtons).findRegion("threeBtn"))));
+		threeBtn = new Button(threeBtnStyle);
+		threeBtn.setPosition((Gdx.graphics.getWidth()/2-(threeBtn.getWidth()/2)), ((Gdx.graphics.getHeight()/2)-(threeBtn.getHeight()/2)));
 	}
 	
 	public void createBackground() {
