5e34c02135dcc0d4a300ebc9855c4eef0d08a181
Bayani Julian <bayanijulian@gmail.com>
2014-07-09 22:03:43 -0500

    created animations for entites

--- core/src/com/hypetrainstudios/dontmiss/entity/Entity.java
+++ core/src/com/hypetrainstudios/dontmiss/entity/Entity.java
@@ -1,7 +1,9 @@
 package com.hypetrainstudios.dontmiss.entity;
 
 import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Circle;
 
 public abstract class Entity{
@@ -13,6 +15,11 @@ public abstract class Entity{
 	protected float y;
 	protected float alpha;
 
+	
+	protected Animation birthAnimation;
+	protected Animation livingAnimation;
+	protected Animation deathAnimation;
+	protected float timeAnimation;
 	public Entity(Sprite spr){
 		this.spr = spr;
 		this.active = true;
@@ -22,9 +29,46 @@ public abstract class Entity{
 		collisionBounds = new Circle(x,y,(spr.getHeight()/2));
 		spr.setAlpha(alpha);
 		spr.setColor(1, 1, 1, 1);
+		birthAnimation = null;
+		livingAnimation = null;
+		deathAnimation = null;
+		timeAnimation = 0;
 	}
 	
 	public abstract void update(float delta);
+	
+	public void createBirthAnimation(int amountOfFrames, TextureRegion[] frames,PlayMode mode){
+		birthAnimation = new Animation(1f/amountOfFrames,frames);
+		timeAnimation = 0;
+		birthAnimation.setPlayMode(mode);
+	}
+	public void createLivingAnimation(int amountOfFrames, TextureRegion[] frames,PlayMode mode){
+		livingAnimation = new Animation(1f/amountOfFrames,frames);
+		timeAnimation = 0;
+		livingAnimation.setPlayMode(mode);
+	}
+	public void createDeathAnimation(int amountOfFrames, TextureRegion[] frames,PlayMode mode){
+		deathAnimation = new Animation(1f/amountOfFrames,frames);
+		timeAnimation = 0;
+		deathAnimation.setPlayMode(mode);
+	}
+	public void updateBirthAnimation(float delta){
+		spr.setRegion(birthAnimation.getKeyFrame(timeAnimation));
+		if(birthAnimation.isAnimationFinished(timeAnimation))	timeAnimation = 0;
+		else	timeAnimation += delta;
+	}
+	public void updateLivingAnimation(float delta){
+		spr.setRegion(livingAnimation.getKeyFrame(timeAnimation));
+		if(livingAnimation.isAnimationFinished(timeAnimation))	timeAnimation = 0;
+		else	timeAnimation += delta;
+	}
+	public void updateDeathAnimation(float delta){
+		spr.setRegion(deathAnimation.getKeyFrame(timeAnimation));
+		if(deathAnimation.isAnimationFinished(timeAnimation))	timeAnimation = 0;
+		else	timeAnimation += delta;
+	}
+	
+	
 	
 	public void updateBounds(){
 		collisionBounds.setPosition(spr.getX()+ (spr.getWidth()/2)
--- core/src/com/hypetrainstudios/dontmiss/handlers/CollisionHandler.java
+++ core/src/com/hypetrainstudios/dontmiss/handlers/CollisionHandler.java
@@ -9,17 +9,23 @@ import com.hypetrainstudios.dontmiss.challenges.Challenge;
 
 public class CollisionHandler {
 	public static void update(){
+		
 		//Checks collision between projectile and enemies
-			for (int x=0; x<Creator.projectiles.size();x++)
-				for (int k=0; k<Creator.enemies.size();k++)
-					if( Creator.projectiles.get(x).getCircle().overlaps( Creator.enemies.get(k).getCircle() ) )
-						enemyCollision(k,x);
-			
-			//Checks collision between projectiles and the player
-			for(int  x= 0; x<Creator.enemies.size();x++)
-				if(Creator.enemies.get(x).getCircle().overlaps(Creator.player.getCircle()))
-					playerCollision();
-			
+		for (int x=0; x<Creator.projectiles.size();x++)
+			for (int k=0; k<Creator.enemies.size();k++)
+				if( Creator.projectiles.get(x).getCircle().overlaps( Creator.enemies.get(k).getCircle() ) )
+					enemyCollision(k,x);
+		
+		//Checks collision between projectiles and the player
+		for(int  x= 0; x<Creator.enemies.size();x++)
+			if(Creator.enemies.get(x).getCircle().overlaps(Creator.player.getCircle()))
+				playerCollision();
+		
+		//Checks collision between projectile and bonuses
+				for (int x=0; x<Creator.projectiles.size();x++)
+					for (int k=0; k<Creator.bonuses.size();k++)
+						if( Creator.projectiles.get(x).getCircle().overlaps( Creator.bonuses.get(k).getCircle() ) )
+							bonusCollision(k,x);
 	}
 	
 	private static void enemyCollision(int k, int x){
@@ -30,6 +36,11 @@ public class CollisionHandler {
 		//sends a signal to the challenge class saying there has been a collision (essentially)
 		Challenge.currentCode = Challenge.codeCollision;
 	}
+	
+	private static void bonusCollision(int k, int x){
+		
+	}
+	
 	private static void playerCollision(){
 		//puts the game in a game over state
 		Creator.gameOver = true;
